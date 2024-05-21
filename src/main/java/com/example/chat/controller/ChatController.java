48dfede9171028eca1725a6dce534b6819939494
2251341 <dydwksos246@naver.com>
2024-05-21 17:01:13 +0900

    웹소켓 연결

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.example.chat.controller;
+
+import com.example.chat.model.ChatRoom;
+import com.example.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam(name = "name") String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
---
+++ src/main/java/com/example/chat/handler/WebSockChatHandler.java
@@ -0,0 +1,35 @@
+package com.example.chat.handler;
+
+import com.example.chat.model.ChatMessage;
+import com.example.chat.model.ChatRoom;
+import com.example.chat.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSockChatHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+// 삭제        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^ ");
+// 삭제       session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+    }
+
+}
