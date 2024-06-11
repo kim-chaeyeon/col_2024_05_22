1a280a28059b9c5ca4c7d51785cad9c26832dbf1
EoGeunHyeok <eghsamo@naver.com>
2024-06-11 14:36:57 +0900

    ìmyPage,modify.html 수정

---
+++ src/main/resources/js/Swiper.js
@@ -0,0 +1,18 @@
+<div class="header-slider swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="https://i.ibb.co/kHFkNNr/001-removebg-preview.png" alt="Slide 1" class="fill-parent">
+        </div>
+        <div class="swiper-slide">
+            <img src="https://i.ibb.co/m59GCsF/image.jpg" alt="Slide 2">
+        </div>
+        <div class="swiper-slide">
+            <img src="https://i.ibb.co/BVmSBkn/image.jpg" alt="Slide 3">
+        </div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- Add Navigation -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+</div>
